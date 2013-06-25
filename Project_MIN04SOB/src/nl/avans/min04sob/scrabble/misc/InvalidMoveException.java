31d1c5614d573681334314521fdba3722a35c549
aaron kieboom <aja.kieboom@student.avans.nl>
2013-06-25 11:11:14 +0200

    Zorgen dat je niet legwoord kunt doen als het niet jouw beurt is

--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/misc/InvalidMoveException.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/misc/InvalidMoveException.java
@@ -12,11 +12,12 @@ public class InvalidMoveException extends Exception {
 	public static final int STATE_DENIED = 6;
 	public static final int STATE_PENDING = 7;
 	public static final int STATE_SETPENDING = 8;
+	public static final int STATE_NOTYOURTURN = 9;
 	private int errorType;
 
 	public InvalidMoveException(int error) {
 		if (ArrayUtils.contains(new int[] { NOT_ALIGNED, NOT_CONNECTED,
-				NOT_ON_START, NO_LETTERS_PUT, STATE_NOT_ATTACHED, STATE_DENIED, STATE_PENDING, STATE_SETPENDING}, error)) {
+				NOT_ON_START, NO_LETTERS_PUT, STATE_NOT_ATTACHED, STATE_DENIED, STATE_PENDING, STATE_SETPENDING, STATE_NOTYOURTURN}, error)) {
 			errorType = error;
 			return;
 		}
@@ -40,6 +41,8 @@ public class InvalidMoveException extends Exception {
 			return "Woord is al voorgesteld";
 		case STATE_SETPENDING :
 			return "Woord wordt voorgesteld";
+		case STATE_NOTYOURTURN :
+			return "Het is uw beurt niet";
 		case NO_LETTERS_PUT:
 		default:
 			return "Er zijn geen letters neergelegd.";
