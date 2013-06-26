f254573117be1e4906ed3e0e2d3e5c029f49e9a2
japguele <japguele@student.avans.nl>
2013-06-26 13:41:14 +0200

    playertiles stuff

--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/models/GameModel.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/models/GameModel.java
@@ -198,8 +198,8 @@ public class GameModel extends CoreModel {
 
 		StashModel stash = new StashModel();
 
-		Tile[] letters = stash.getPlayerTiles(currentUser, this);
-
+		Tile[] letters = stash.getPlayerTiles(currentUser, this,this.getLastTurn());
+		System.out.println(letters.length);
 		Tile[] newletters = new Tile[7];
 
 		for (int counter = 0; counter < newletters.length; counter++) {
@@ -209,17 +209,20 @@ public class GameModel extends CoreModel {
 
 					newletters[counter] = stash.getRandomLetter(
 							this.getGameId(), turnid);
-					stash.addToPlankje(this.gameId,
-							newletters[counter].getTileId(), turnid);
+					
 
 				}
 			} else {
 
 				newletters[counter] = letters[counter];
 			}
+			
 
 		}
-
+		for(Tile tile : newletters){
+			stash.addToPlankje(this.gameId,
+					tile.getTileId(), turnid);
+		}
 		boardPanel.setPlayerTiles(newletters);
 	}
 
@@ -241,6 +244,9 @@ public class GameModel extends CoreModel {
 
 	public int getCurrentobserveturn() {
 		return currentobserveturn;
+	}
+	public int getLastTurn(){
+		return this.lastTurn;
 	}
 
 	public void setBoardModel(BoardModel model) {
